1814f328489566a68575e18313e61986a3196f69
root <jepcele@yahoo.com>
2023-09-07 11:54:01 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - script that compiles a C file and creates an executable named cisfun
  *
- * return: returns 0
+ * Return: returns 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
