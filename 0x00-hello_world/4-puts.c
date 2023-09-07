3328b88baaf266346fad0a7b7161bb121b862655
root <jepcele@yahoo.com>
2023-09-07 11:51:41 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - script that compiles a C file and creates an executable named cisfun
+ *
+ * return: returns 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
