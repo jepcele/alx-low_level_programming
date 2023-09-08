e074222172718b9304d2f925e813bf70df2aa66f
root <jepcele@yahoo.com>
2023-09-08 13:07:09 -0700

    combi

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 47, j = 48;
+
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			if (i == j)
+				continue;
+			putchar(i);
+			putchar(j);
+			if (i == )
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+}
