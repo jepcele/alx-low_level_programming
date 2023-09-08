fd944db806b120762d3212a21193510cbe8b971a
root <jepcele@yahoo.com>
2023-09-08 10:33:39 -0700

    print numz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line.
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+        int nums = 48;
+
+        while (nums < 58)
+        {
+                putchar(nums);
+                nums++;
+        }
+	putchar('\n');
+	return (0);
+}
