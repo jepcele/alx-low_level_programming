e5c6b2e5c8d12bc773d10ed147932b3111eebf1d
root <jepcele@yahoo.com>
2023-09-12 18:36:16 -0700

    small

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: 0
  */
-int main(void)
+void print_alphabet(void)
 {
 	char alp = 'a'; 
 
@@ -16,5 +16,5 @@ int main(void)
 			break;
 	}
 	_putchar('\n');
-	return (0);
+	return ;
 }
