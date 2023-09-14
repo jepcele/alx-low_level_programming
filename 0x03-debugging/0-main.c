4595185124ce15afa363cc7879e096779a5bdcf8
root <jepcele@yahoo.com>
2023-09-14 05:51:23 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,11 @@
 
 int main(void)
 {
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+	int i;
+	
+	i = 98;
+	if (i == 0)
+		positive_or_negative(i);
+	
+	return (0);
 }
