abeecc69a328e39c61b515876ca362eee53ecc61
root <jepcele@yahoo.com>
2023-09-13 19:23:10 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * times_table -  prints the 9 times table, starting with 0
  *
@@ -11,11 +12,11 @@ void times_table(void)
 	while (i < 58)
 	{
 		j = 48;
-		
+
 		while (j < 58)
 		{
 			prod = i * j;
-			_putchar('0' + prod);
+			prinf("%d", prod);
 			if (i < 57)
 			{
 				_putchar(44);
