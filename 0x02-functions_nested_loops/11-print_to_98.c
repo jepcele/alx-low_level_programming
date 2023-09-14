a895352a99ae9520b4f117d4c89b089fec7242f9
root <jepcele@yahoo.com>
2023-09-13 18:10:53 -0700

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,6 +11,7 @@
 void print_to_98(int n)
 {
 	int i = n;
+
 	if (n < 99)
 	{
 		while (i < 99)
@@ -29,12 +30,12 @@ void print_to_98(int n)
 		while (i > 97)
 		{
 			printf("%d", i);
-                        if (i > 98)
-                        {
-                                putchar(44);
-                                putchar(32);
-                        }
-                        i--;
+			if (i > 98)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			i--;
 		}
 	}
 	putchar('\n');
