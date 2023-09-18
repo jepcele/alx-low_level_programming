e77379f9bffb48ba8f18fff1d1059c7ea7925dde
root <jepcele@yahoo.com>
2023-09-18 01:03:47 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,13 @@ void print_rev(char *s)
 {
 	int len = 0;
 
-	while (*s != '\0')
+	while (*(s + len) != '\0')
 	{
 		len++;
-		s++;
 	}
 	while (len > 0)
 	{
-		_putchar(*(s - 1));
+		_putchar(*(s + len));
 		len--;
 	}
 	_putchar('\n');
