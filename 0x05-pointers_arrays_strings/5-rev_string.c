30e99e2b5a5fd47c8ac292fc9835f1e13c20905f
root <jepcele@yahoo.com>
2023-09-19 19:14:07 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ * @s: string to reverse.
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		if (*(s + len + 1) == '\0')
+		{
+
+		}
+	}
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: string to print
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
+}
