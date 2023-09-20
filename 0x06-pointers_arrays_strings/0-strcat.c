eaaf89283fe4d1551f18f5c80bb10baf5c0e2eea
root <jepcele@yahoo.com>
2023-09-20 07:18:56 -0700

    concartinate string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to copy to.
+ * @src: string to copy.
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+
+}
