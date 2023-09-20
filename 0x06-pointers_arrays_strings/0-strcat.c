f54a1751c9692509f3d67a002615c41f82204618
root <jepcele@yahoo.com>
2023-09-20 16:04:43 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,5 +8,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int i = 0, j;
 
+	while (*(dest + i) != '\0')
+	{
+		if (*(dest + i + 1) == '\0')
+		{
+			j = 0;
+			while (*(src + j) != '\0')
+			{
+				*(dest + i + 1) = *(src + j);
+				if (*(src + j + 1) == '\0')
+				{
+					*(dest + i + 2) = '\0';
+				}
+				j++;
+				i++;
+			}
+		}
+		i++;
+	}
 }
