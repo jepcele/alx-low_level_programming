f127b4d3affacaaed3060b316288d6b433484906
root <jepcele@yahoo.com>
2023-09-20 19:40:16 -0700

    encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,4 @@
-#include main.h
+#include "main.h"
 /**
  * leet -  function that encodes a string into 1337.
  * @s: string to encode
@@ -7,12 +7,20 @@
  */
 char *leet(char *s)
 {
-	int i = 0;
+	int i = 0, j;
 	char arr[5][2] = {{'a', 'A'}, {'e', 'E'}, {'o', 'O'}, {'t', 'T'}, {'l', 'L'}};
 	char code[5] = {'4', '3', '0', '7', '1'};
 
 	while(*(s + i) != '\0')
 	{
-		
+		j = 0;
+		while ( j < 5)
+		{
+			if (*(s + i) == arr[j][0] || *(s + i) == arr[j][1])
+				*(s + i) = code[j];
+			j++;
+		}
+		i++;
 	}
+	return (s);
 }
