259add71263ff73e8a518a6f77f8a174fe4fd653
root <jepcele@yahoo.com>
2023-09-27 17:44:36 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,12 +7,12 @@
  */
 int factorial(int n)
 {
+	int sum = 0;
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (1);
 	if (n > 0)
-	{
-		return n * factorial(n - 1);
-	}
+		sum += n * factorial(n - 1);
+	return (sum);
 }
