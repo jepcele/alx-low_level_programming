65912e6b428c999bbf09a91abfce66b3f979c71d
root <jepcele@yahoo.com>
2023-10-01 21:59:04 -0700

    duplicate string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,5 +11,38 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int i, j;
+	char *p;
 
+	i = 0;
+	while (*(s1 + i) != '\0')
+	{
+		if (*(s1 + i + 1) == '\0')
+		{
+			j = 0;
+			while (*(s2 + j) != '\0')
+			{
+				j++;
+				i++;
+			}
+		}
+		i++;
+	}
+	p = malloc(i);
+	if (!p)
+		return (NULL);
+	i = 0;
+	while (*(s1 + i) != '\0')
+	{
+		*(p + i) = *(s1 + i);
+
+		j = 0;
+		while (*(s2 + j) != '\0')
+		{
+			*(p + i) = *(s2 + j);
+			i++;
+			j++;
+		}
+		i++;
+	}
 }
