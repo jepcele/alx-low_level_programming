a2848160c7b6d124da31e9a4f0be423f9320e6ff
root <jepcele@yahoo.com>
2023-10-01 21:27:47 -0700

    duplicate string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return:  returned pointer should point to a newly allocated
+ * space in memory which contains the contents of s1, followed
+ * by the contents of s2, and null terminated
+ */
+char *str_concat(char *s1, char *s2)
+{
+
+}
