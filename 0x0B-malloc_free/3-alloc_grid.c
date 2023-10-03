10dee29e78e3443a0e602ba529380f4e691d3386
root <jepcele@yahoo.com>
2023-10-03 16:45:37 -0700

    allocate grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,7 +4,7 @@
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
  * @width: width of the array
- * @heigh: height of the array
+ * @height: height of the array
  *
  * Return: returns a pointer to a 2 dimensional array of integers.
  */
