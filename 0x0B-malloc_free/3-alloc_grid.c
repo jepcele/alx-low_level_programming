a30ac05197a1ba7c8c16ee4f84b08a055cb4024b
root <jepcele@yahoo.com>
2023-10-03 16:49:35 -0700

    allocate grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p = malloc(sizeof(width * height));
+	p = malloc(width * height);
 
 	if (!p)
 		return (NULL);
