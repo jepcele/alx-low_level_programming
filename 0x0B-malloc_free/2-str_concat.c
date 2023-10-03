33ea9c76a54cc514fd159e6814c666e994d0a428
root <jepcele@yahoo.com>
2023-10-03 06:56:46 -0700

    string duplicate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 
 	if (s1 == NULL)
-		*s1 = "";
+		*s1 = (char) "";
 	if (s2 == NULL)
-		*s2 = "";
+		*s2 = (char) "";
 	i = 0;
 	while (*(s1 + i) != '\0')
 	{
