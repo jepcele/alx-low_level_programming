74ddd42b2620565ca31f0025a560ef2772bc3cad
root <jepcele@yahoo.com>
2023-10-03 19:50:21 -0700

    allocate grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - frees a 2 dimensional grid previously
+ * created by your alloc_grid function.
+ * @grid: two dimentional grid
+ * @height: height of the grid
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+
+}
