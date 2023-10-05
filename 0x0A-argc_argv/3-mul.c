7d10ed46074c5189e85a7b7f7caf0149e0b9fab0
root <jepcele@yahoo.com>
2023-10-04 18:06:15 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies two numbers.
  * @argc: argument count
@@ -8,7 +9,7 @@
  */
 int main(int argc, char **argv)
 {
-	int i, sum = 0, val1 = 0, val2 = 0;
+	int sum = 0, val1 = 0, val2 = 0;
 
 	if (argc < 3)
 	{
@@ -17,8 +18,8 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		val1 = atoi(arg[1]);
-		val2 = atoi(arg[2]);
+		val1 = atoi(argv[1]);
+		val2 = atoi(argv[2]);
 		sum = val1 * val2;
 		printf("%d\n", sum);
 	}
